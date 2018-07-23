0ac83d71ef75914517be2e7af01c7edf05468def
Brian Ramirez <brianramirez482@gmail.com>
2018-07-22 20:15:28 -0500

    [feat]: privatizing routes that require auth
      - PrivateRoute component created
      - Home route privatized
    
    #Resolves: #
    #See als

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './complements/PrivateRoute';
 import './App.css';
 // Styles
 // CoreUI Icons Set
@@ -25,11 +26,11 @@ class App extends Component {
     return (
       <HashRouter>
         <Switch>
+          <PrivateRoute path="/" name="Home" component={DefaultLayout} />
           <Route exact path="/login" name="Login Page" component={Login} />
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
-          <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route path="/" name="Home" component={DefaultLayout} />
+          <Route exact path="/500" name="Page 500" component={Page500} />          
         </Switch>
       </HashRouter>
     );
