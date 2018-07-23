898e75cc65399e1ecc8715a20dc05b91139c6422
Juan Rodriguez <jsrd98@gmail.com>
2018-07-23 00:45:40 -0500

    [chore]: Merge front

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <Route exact path="/login" name="Login Page" component={Login} />
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
-          <Route exact path="/500" name="Page 500" component={Page500} />          
+          <Route exact path="/500" name="Page 500" component={Page500} />
           <PrivateRoute path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
