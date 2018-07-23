2ae49682d60a071df67e588fc09ddfc0ef2d9e6c
Brian Ramirez <brianramirez482@gmail.com>
2018-07-22 20:03:30 -0500

    [chore]: ui changed
      - core ui implemented
    
    #Resolves: #
    #See also: #c

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,20 +1,11 @@
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import './polyfill'
 import React from 'react';
-import { store, history} from './store';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// disable ServiceWorker
+// import registerServiceWorker from './registerServiceWorker';
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
-
-import App from './components/App';
-
-ReactDOM.render((
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
-
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// disable ServiceWorker
+// registerServiceWorker();
