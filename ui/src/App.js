807f794118855739dae094405617d3cb1de15306
Brian Ramirez <brianramirez482@gmail.com>
2018-07-22 20:18:39 -0500

    [fix]: problem with endpoints fixed
    #  -
    
    #Resolves: #
    #See also: #

--- ui/src/App.js
+++ ui/src/App.js
@@ -25,12 +25,12 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <Switch>
-          <PrivateRoute path="/" name="Home" component={DefaultLayout} />
+        <Switch>          
           <Route exact path="/login" name="Login Page" component={Login} />
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />          
+          <PrivateRoute path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
     );
