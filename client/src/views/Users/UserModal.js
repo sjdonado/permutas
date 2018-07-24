951711d8b3e32072dce87f4975a50b8e57d88cb8
Juan Rodriguez <jsrd98@gmail.com>
2018-07-24 11:51:48 -0500

    [feat]: edit user modal

--- client/src/views/Users/UserModal.js
+++ client/src/views/Users/UserModal.js
@@ -5,29 +5,42 @@ class UserModal extends React.Component {
   constructor(props) {
     super(props);
     console.log(this.props.user);
+    this.state = {
+      edit: false
+    }
   }
+  handleEdit = e => {
+    this.setState(prevState => { edit: !prevState.edit });
+  }
+
   render() {
     return (
-      <Modal isOpen={this.props.open} toggle={this.props.toggle} className={this.props.className}>
+      <Modal isOpen={this.props.open} toggle={this.props.toggle} size="lg" >
         <ModalHeader toggle={this.toggle}>{this.props.user.fullname}</ModalHeader>
         <ModalBody>
           <Table responsive striped hover>
+            <thead>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Número de identificación</th>
+                <th scope="col">Correo electrónico</th>
+                <th scope="col">Telefono</th>
+                <th scope="col">Celular</th>
+              </tr>
+            </thead>
             <tbody>
-              {
-                Object.keys(this.props.user).map((key, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{`${key}:`}</td>
-                      <td><strong>{this.props.user[key]}</strong></td>
-                    </tr>
-                  )
-                })
-              }
+              <tr>
+                <td>{this.state.edit ? <input type="text" name="fullname" value={this.props.user.fullname}></input> : this.props.user.fullname}</td>
+                <td>{this.props.user.dni}</td>
+                <td>{this.props.user.email}</td>
+                <td>{this.props.user.mobilePhone}</td>
+                <td>{this.props.user.phone}</td>
+              </tr>
             </tbody>
           </Table>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={this.props.toggle}>Editar</Button>{' '}
+          <Button color="primary" onClick={this.props.handleEdit}>Editar</Button>
           <Button color="secondary" onClick={this.props.toggle}>Cancelar</Button>
         </ModalFooter>
       </Modal>
