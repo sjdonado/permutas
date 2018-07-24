c39fb6a901f3f3abef2a994c4efc0e81e6899977
Juan Rodriguez <jsrd98@gmail.com>
2018-07-24 08:36:25 -0500

    Cleaned template, localstorage save user

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,34 +1,5 @@
 import React, { Component } from 'react';
-import { Bar, Line } from 'react-chartjs-2';
-import {
-  Badge,
-  Button,
-  ButtonDropdown,
-  ButtonGroup,
-  ButtonToolbar,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  Col,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Progress,
-  Row,
-  Table,
-} from 'reactstrap';
-import Widget03 from '../../views/Widgets/Widget03'
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
-
-const brandPrimary = getStyle('--primary')
-const brandSuccess = getStyle('--success')
-const brandInfo = getStyle('--info')
-const brandWarning = getStyle('--warning')
-const brandDanger = getStyle('--danger')
+import User from '../Users/User'
 
 class Dashboard extends Component {
   constructor(props) {
@@ -38,50 +9,7 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col sm="12">
-            <Card>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="12">
-            <Card>
-              <Table>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+      <User />
     );
   }
 }
--- client/src/views/Pages/Login/Login.js
+++ client/src/views/Pages/Login/Login.js
@@ -2,110 +2,110 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 import {
-  Button,
-  Card,
-  CardBody,
-  CardGroup,
-  Col,
-  Container,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Row
+    Button,
+    Card,
+    CardBody,
+    CardGroup,
+    Col,
+    Container,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    Row
 } from 'reactstrap';
 
 import Requests from '../../../Requests';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      logged: false
-    }        
-  }
-  handleSubmit = e => {
-    e.preventDefault();
-    const doc = {
-      email: this.state.email,
-      password: this.state.password,
-      redirect: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            logged: false
+        }
     }
-    console.log(doc);
-    Requests.post('/users/signin', doc)
-      .then( res => {
-        localStorage.setItem('token', res.data.meta.token);
-        this.setState({ redirect: true });
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to='/' />;
+    handleSubmit = e => {
+        e.preventDefault();
+        const doc = {
+            email: this.state.email,
+            password: this.state.password,
+            redirect: false
+        }
+        Requests.post('/users/signin', doc)
+            .then(res => {
+                localStorage.setItem('token', res.data.meta.token);
+                localStorage.setItem('user', JSON.stringify((res.data.item)));
+                this.setState({ redirect: true });
+            })
+            .catch(err => {
+                console.error(err);
+            });
     }
-    return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-4">
-                  <CardBody>
-                    <h1>Iniciar Sesión</h1>
-                    <p className="text-muted">Iniciar sesión con tu cuenta</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Correo electrónico" name="email" onChange={this.onChange} value={this.state.email} />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Contraseña" name="password" onChange={this.onChange} value={this.state.password} />
-                    </InputGroup>
-                    <Row>
-                      <Col xs="6">
-                        <Button color="primary" className="px-4" onClick={this.handleSubmit}>Iniciar Sesión</Button>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Registrarse</h2>
-                      <p>¡Crea una cuenta!</p>
-                      <Link to="/register">
-                        <Button
-                          color="primary"
-                          className="mt-3"
-                          active >
-                          Crear cuenta
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to='/' />;
+        }
+        return (
+            <div className="app flex-row align-items-center">
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col md="8">
+                            <CardGroup>
+                                <Card className="p-4">
+                                    <CardBody>
+                                        <h1>Iniciar Sesión</h1>
+                                        <p className="text-muted">Iniciar sesión con tu cuenta</p>
+                                        <InputGroup className="mb-3">
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <i className="icon-user"></i>
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="text" placeholder="Correo electrónico" name="email" onChange={this.onChange} value={this.state.email} />
+                                        </InputGroup>
+                                        <InputGroup className="mb-4">
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <i className="icon-lock"></i>
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="password" placeholder="Contraseña" name="password" onChange={this.onChange} value={this.state.password} />
+                                        </InputGroup>
+                                        <Row>
+                                            <Col xs="6">
+                                                <Button color="primary" className="px-4" onClick={this.handleSubmit}>Iniciar Sesión</Button>
+                                            </Col>
+                                        </Row>
+                                    </CardBody>
+                                </Card>
+                                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
+                                    <CardBody className="text-center">
+                                        <div>
+                                            <h2>Registrarse</h2>
+                                            <p>¡Crea una cuenta!</p>
+                                            <Link to="/register">
+                                                <Button
+                                                    color="primary"
+                                                    className="mt-3"
+                                                    active >
+                                                    Crear cuenta
                         </Button>
-                      </Link>
-                    </div>
-                  </CardBody>
-                </Card>
-              </CardGroup>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+                                            </Link>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </CardGroup>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default Login;
